0ec112e0be2f1fc89c523f15ef49c87660565fcd
Tamar khuskivadze <thamuna.khuskivadze@gmail.com>
2025-08-31 20:20:09 +0400

    finished

--- app/FilterToggleButton/FilterToggleButton.tsx
+++ app/FilterToggleButton/FilterToggleButton.tsx
@@ -14,6 +14,7 @@ const FilterToggleButton = () => {
             className="flex items-center gap-2 px-4 py-2 border border-gray-300 rounded-lg hover:bg-gray-50 transition-colors"
         >
             <Filter className="w-4 h-4" />
+            
             Filters
         </button>
     )
